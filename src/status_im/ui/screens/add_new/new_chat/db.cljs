63d6033a00a74dade006edeb3d480f0cd4992691
Andrey Shovkoplyas <motor4ik@gmail.com>
2020-02-06 13:14:15 +0100

    [#9789] Add a loading indicator and button to username input of start new chat

--- src/status_im/ui/screens/add_new/new_chat/db.cljs
+++ src/status_im/ui/screens/add_new/new_chat/db.cljs
@@ -1,19 +1,11 @@
 (ns status-im.ui.screens.add-new.new-chat.db
-  (:require [status-im.utils.hex :as hex]
-            [status-im.utils.platform :as platform]
-            [status-im.i18n :as i18n]
-            [cljs.spec.alpha :as spec]
-            [clojure.string :as string]))
+  (:require [cljs.spec.alpha :as spec]))
 
 (defn own-public-key?
   [{:keys [multiaccount]} public-key]
   (= (:public-key multiaccount) public-key))
 
 (defn validate-pub-key [db public-key]
-  (cond
-    (not (spec/valid? :global/public-key public-key))
-    (i18n/label (if platform/desktop?
-                  :t/use-valid-contact-code-desktop
-                  :t/use-valid-contact-code))
-    (own-public-key? db public-key)
-    (i18n/label :t/can-not-add-yourself)))
+  (or
+   (not (spec/valid? :global/public-key public-key))
+   (own-public-key? db public-key)))
